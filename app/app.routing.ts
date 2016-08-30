192c61fff421dddc346fb7a6f0d51b8fff5dc5ba
RicoLiu <2869183609@qq.com>
2016-08-30 20:13:14 +0800

    route dashboard

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,11 +1,28 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent } from "./dashboard.component";
+import { HeroesComponent } from './heroes.component';
+import { HeroesDetailComponent } from './hero-detail.component'
 
 const appRoutes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMath: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'detail/:id',
+        component: HeroesDetailComponent
+    },
     {
         path: 'heroes',
         component: HeroesComponent
     }
 ];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes)
