8ed3ec930835cff75fc591293cba9982e2dcdcc4
RicoLiu <2869183609@qq.com>
2017-02-07 14:35:35 +0800

    delete

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -24,7 +24,6 @@ export class DashboardComponent implements OnInit {
     }
 
     gotoDetail(hero: Hero): void {
-        let link = ['/detail', hero.id]
-        this.router.navigate(link)
+        this.router.navigate(['/detail', hero.id])
     }
 }
