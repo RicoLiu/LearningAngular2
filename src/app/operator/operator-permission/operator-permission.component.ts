88bea265bcb58bb01cdaf3c5f977ee3ab31baf35
RicoLiu <2869183609@qq.com>
2017-03-28 17:01:07 +0800

    完善基本增删改查

--- src/app/operator/operator-permission/operator-permission.component.ts
+++ src/app/operator/operator-permission/operator-permission.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { flyIn } from '../../animations/fly-in';
 
 import { OperatorService } from './services/operator.service'
@@ -15,15 +15,20 @@ import { OperatorService } from './services/operator.service'
 
 export class OperatorPermissionComponent implements OnInit {
     operators: string[];
+    operator: string;
+    modify_Operator: string;
+    oldOperator: string;
     showOperator: Boolean;
+    modifyOperator: Boolean;
 
-    constructor(private operatorService: OperatorService) { 
+    constructor(private operatorService: OperatorService) {
         this.operators = operatorService.getOperatorService();
         this.showOperator = true;
+        this.modifyOperator = false;
     }
 
     ngOnInit() {
-        this.operators =  this.operatorService.getOperatorService();
+        this.operators = this.operatorService.getOperatorService();
     }
 
     ToggleShow() {
@@ -32,11 +37,34 @@ export class OperatorPermissionComponent implements OnInit {
 
     AddOperator(operator) {
         this.operators.push(operator);
+        this.operatorService.addOperator(operator);
     }
 
     DeleteOperator(i) {
         this.operators.splice(i, 1);
+        this.operatorService.deleteOperator(i);
     }
 
-    
+    EditOperator(operator) {
+        this.modify_Operator = operator;
+        this.oldOperator = operator;
+        this.modifyOperator = true;
+    }
+
+    ModifyOperator() {
+        for (let i = 0; i < this.operators.length; i++) {
+            if (this.operators[i] === this.oldOperator) {
+                this.operators[i] = this.modify_Operator;
+                console.log('modify  operator ............');
+                this.operatorService.modify(this.modify_Operator, i);
+            }
+        }
+    }
+
+    doSomething(newValue) {
+        console.log(newValue);
+        this.modifyOperator = newValue !== '';
+    }
+
+
 }
--- src/app/operator/operator-permission/services/operator.service.ts
+++ src/app/operator/operator-permission/services/operator.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Init } from '../init-operator'
+import { Init } from '../init-operator';
 
 @Injectable()
 export class OperatorService extends Init {
@@ -9,8 +9,29 @@ export class OperatorService extends Init {
     }
 
     getOperatorService(): string[] {
-        const operators = JSON.parse(localStorage.getItem('operators'));
+        let operators = JSON.parse(localStorage.getItem('operators'));
 
         return operators;
+    }
+
+    addOperator(operator) {
+        let operators = JSON.parse(localStorage.getItem('operators'));
+        operators.push(operator);
+
+        localStorage.setItem('operators', JSON.stringify(operators));
+    }
+
+    deleteOperator(i) {
+        let operators = JSON.parse(localStorage.getItem('operators'));
+        operators.splice(i, 1);
+
+        localStorage.setItem('operators', JSON.stringify(operators));
+    }
+
+    modify(operator, i) {
+        let operators = JSON.parse(localStorage.getItem('operators'));
+        operators[i] = operator;
+
+        localStorage.setItem('operators', JSON.stringify(operators));
     }
 }
