3326dfa5fbec4e868a74cdd08336068a25c38081
RicoLiu <2869183609@qq.com>
2017-04-27 14:49:12 +0800

    更新 angular v4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { HttpModule,JsonpModule ,Http} from '@angular/http';
+import { HttpModule, JsonpModule, Http} from '@angular/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
 import {ToastModule} from 'ng2-toastr/ng2-toastr';
@@ -37,6 +38,7 @@ export function createTranslateLoader(http: Http) {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     RouterModule,
     ReactiveFormsModule,
     HttpModule,
