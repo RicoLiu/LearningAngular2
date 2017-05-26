779483f8b0cccc7c3f15c3daa0e4dede895da431
RicoLiu <2869183609@qq.com>
2017-05-26 10:43:53 +0800

    测试持续集成

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,7 +13,7 @@ describe('NiceFish App', function() {
 	  //这里的browser对象是由Protractor提供的，大家可以把下面一行注释放开，看看这个对象的内容
 	  //console.log(browser);
 	  //首先利用browser.get()这个接口让浏览器打开app.index.html这个页面
-	  browser.get('src/index.html');
+	  // browser.get('src/index.html');
 	  //然后用browser.getLocationAbsUrl()获取到地址栏的URL，看看是不是转向了/phones这个路径
 	//   browser.getCurrentUrl().then(function(url) {
 	//       expect(url.split('#')[1]).toBe('/phones');
